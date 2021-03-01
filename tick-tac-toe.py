b3f7e875294f6cc9ddec3b0818089c72a4787f17
AA122AA <gromartem2000@gmail.com>
2021-03-01 16:51:02 +0300

    start tic-tac-toe

---
+++ tick-tac-toe.py
@@ -0,0 +1,11 @@
+class Field:
+    row
+a = input("Крестики нолики. Кто ходит первым? 1 игрок, 2 компьютер")
+if a == 1:
+    b = input("Введите номер поля")
+else:
+    comp = "A1"
+    print(comp)
+
+while b!=0:
+    b = input("Введите номер поля")
